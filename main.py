dddf95e0dbb57dac229cde1a02b4b1cecd55d44f
romanryzh09 <romkinapochta2017@mail.ru>
2024-04-07 11:58:03 +0300

    Изменение диапозона чисел

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ points = {
     "player": 0
 }
 while True:
-    a = random.randint(10, 90)
-    b = random.randint(10, 90)
+    a = random.randint(10, 120)
+    b = random.randint(10, 120)
     op = random.choice(("+", "-"))
     if op == "+":
         ans = a+b
