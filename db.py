e54a5e96961b5ef5f361a471b9198dbab0ba0846
romanryzh09 <romkinapochta2017@mail.ru>
2024-04-17 20:46:03 +0300

    Добавление таблицы "Кошки".

--- db.py
+++ db.py
@@ -17,11 +17,18 @@ class Users(Base):
     hp = Column(Integer, default=100)
     damage = Column(Integer, default=20)
 
+class Cats(Base):
+    __tablename__ = "cats"
+    id = Column(Integer, primary_key= True, nullable= False)
+    name = Column(String)
+    color = Column(String)
+
 Session = sessionmaker(bind= engine)
 s = Session()
 
-hero = Users(id= 123, name= "Саня Окунь")
-s.add(hero)
+hero = Users(id= 123, name= "Саня Карась")
+# s.add(hero)
+s.merge(hero)
 s.commit()
 
 
