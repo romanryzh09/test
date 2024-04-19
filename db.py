de5cf33c10ba7ebc3c018cb84cee672ce9a6662b
romanryzh09 <romkinapochta2017@mail.ru>
2024-04-19 23:58:03 +0300

    Создание новой таблицы, добавление в нее некоторых данных. Убраны лишние строки кода.

--- db.py
+++ db.py
@@ -27,25 +27,38 @@ class Cats(Base):
     name = Column(String)
     color = Column(String)
 
+class Enemies(Base):
+    __tablename__ = "enemies"
+    id = Column(Integer, primary_key= True, nullable= False)
+    name = Column(String)
+    hp = Column(Integer, default= 90)
+    damage = Column(Integer, default= 15)
+
 Session = sessionmaker(bind= engine)
 s = Session()
+
+enem = Enemies(id= 101, name= "Окунь")
+enem1 = Enemies(id= 102, name= "Сом")
+s.merge(enem)
+s.merge(enem1)
+s.commit()
 
 # hero = Users(id= 123, name= "Саня Карась")
 # s.add(hero)
 # s.merge(hero)
 # s.commit()
 
-hero1 = Users(id= 400, name= "Палыч Шиномонтаж")
-hero2 = Users(id= 401, name= "Саныч Окунь")
-hero3 = Users(id= 402, name= "Сергеич Кедровый")
-hero4 = Users(id= 403, name= "Олегыч Бутылка")
-hero5 = Users(id= 404, name= "Дмитрич Резина")
-s.merge(hero1)
-s.merge(hero2)
-s.merge(hero3)
-s.merge(hero4)
-s.merge(hero5)
-s.commit()
+# hero1 = Users(id= 400, name= "Палыч Шиномонтаж")
+# hero2 = Users(id= 401, name= "Саныч Окунь")
+# hero3 = Users(id= 402, name= "Сергеич Кедровый")
+# hero4 = Users(id= 403, name= "Олегыч Бутылка")
+# hero5 = Users(id= 404, name= "Дмитрич Резина")
+# s.merge(hero1)
+# s.merge(hero2)
+# s.merge(hero3)
+# s.merge(hero4)
+# s.merge(hero5)
+# s.commit()
 
 
 
